5ad915351fedddaf7f914c24418849f9eb64b5fc
Vasily Khoruzhick <anarsoul@gmail.com>
2025-05-07 20:36:57 -0700

    dot: remove '. ' from version string

--- dot-command/dot.asm
+++ dot-command/dot.asm
@@ -4,7 +4,7 @@
 text
     include "modules/version.asm"
     jp start
-ver db "zxmit v. ", VERSION_STRING, 13
+ver db "zxmit v", VERSION_STRING, 13
     db "(c) 2022 Alex Nihirash", 13
     db "(c) 2025 Vasily Khoruzhick", 13, 13, 0
 
